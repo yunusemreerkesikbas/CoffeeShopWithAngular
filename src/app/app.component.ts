29d0342c5f3823826172af5b9f5411081ea2183d
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2022-05-13 21:57:03 +0300

    navbar and carousel banner added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { repeat } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[NgbCarouselConfig]
 })
 export class AppComponent {
   title = 'CoffeeShop';
+  images = [
+    {title:'Hasça Kuruyemiş',src:"../assets/images/home-img.png " },
+    {title: 'Hasça Kahve', src: "../assets/images/home-img-2.png"}
+  ]
+  constructor(config:NgbCarouselConfig){
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { CategoryComponent } from './category/category.component';
+import { NavComponent } from './nav/nav.component';
+import { ServicesComponent } from './services/services.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NavComponent,
+    ServicesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
